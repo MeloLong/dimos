3ad2b6e6917b0093c15f1272f7f2164a013b588f
Markus.Liang <69946955+T-Markus-Liang@users.noreply.github.com>
2026-07-18 10:25:01 +0800

    fix(nav-record): match navigation goal message type

--- dimos/navigation/nav_stack/modules/nav_record/nav_record.py
+++ dimos/navigation/nav_stack/modules/nav_record/nav_record.py
@@ -28,7 +28,6 @@ from dimos.msgs.geometry_msgs.Twist import Twist
 from dimos.msgs.nav_msgs.Odometry import Odometry
 from dimos.msgs.nav_msgs.Path import Path as NavPath
 from dimos.msgs.sensor_msgs.PointCloud2 import PointCloud2
-from dimos.msgs.std_msgs.Bool import Bool
 from dimos.msgs.std_msgs.Int8 import Int8
 
 
@@ -61,7 +60,7 @@ class NavRecord(Recorder):
     # LocalPlanner details
     effective_cmd_vel: In[Twist]
     slow_down: In[Int8]
-    goal_reached: In[Bool]
+    goal_reached: In[LcmBool]
 
     # Point clouds
     terrain_map: In[PointCloud2]
